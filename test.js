a638dcfa726b5812b92169fc3925036f854d13b1
laggingreflex <laggingreflex@gmail.com>
2016-12-07 22:03:02 +0530

    Doesn't work unless strict mode is used

--- test.js
+++ test.js
@@ -3,9 +3,11 @@ import test from 'ava';
 import fixture from './fixture';
 import fixture2 from './fixture2';
 import fixture3 from './fixture3';
+import fixture4 from './fixture4';
 
 test(t => {
 	t.is(path.basename(fixture()), 'test.js');
 	t.is(path.basename(fixture2()), 'test.js');
 	t.is(path.basename(fixture3()), 'test.js');
+	t.is(path.basename(fixture4()), 'test.js');
 });
